c3602bf98c516c7c0c31011ee1afc19d70707bca
Òscar Muntal <oscar.muntal@gmail.com>
2023-03-10 21:22:25 +0100

    [UI]: Setting up the Events list UI

---
+++ GithubEvents/Views/EventTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  EventTableViewCell.swift
+//  GithubEvents
+//
+//  Created by Òscar Muntal on 10/3/23.
+//
+
+import UIKit
+
+class EventTableViewCell: UITableViewCell {
+    @IBOutlet weak var loginLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var repoNameLabel: UILabel!
+}
---
+++ GithubEvents/Views/EventsTableViewController.swift
@@ -0,0 +1,28 @@
+//
+//  EventsTableViewController.swift
+//  GithubEvents
+//
+//  Created by Òscar Muntal on 10/3/23.
+//
+
+import Foundation
+import UIKit
+
+class EventsTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "EventTableViewCell", for: indexPath) as? EventTableViewCell else { fatalError("EventTableViewCell not found") }
+        
+        
+        return cell
+    }
+}
